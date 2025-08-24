188c1ba399639ce7b4f4e284f469a8b7bdcca5de
cedric <cedric@sigrist.ch>
2025-08-24 10:57:16 +0200

    added toggleterm

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,16 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*', -- use latest release
+  config = function()
+    require('toggleterm').setup {
+      -- config options (tweak as you like)
+      size = 15,
+      open_mapping = [[<C-\>]],
+      shade_terminals = true,
+      direction = 'horizontal', -- "vertical" | "tab" | "float"
+    }
+
+    -- Example keymap: toggle floating terminal with <leader>tt
+    vim.keymap.set('n', '<leader>tt', '<cmd>ToggleTerm direction=float<CR>', { desc = 'Toggle floating terminal' })
+  end,
+}
